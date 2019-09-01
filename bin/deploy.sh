53c2c1ac8ca2d87466955d6c399f7427f0abaf7d
Etienne Gauvin <etienne@gauvin.info>
2019-09-01 23:38:51 +0200

    Amélioration des scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,11 +1,13 @@
-pwd
+#!/bin/bash
+set -e
 
 # Générer les cartes et les déplacer
-#npm run generate-map
+npm run generate-blockmap-images
+npm run import-blockmap-images
 
 # Transpiler les sources du viewer
-#npm run build
+npm run build
 
 # Déplacer le build dans le dossier du serveur web
-#rm -r /www/data/*
-#mv ../
+rm -r /www/data/*
+mv build/* /www/data
